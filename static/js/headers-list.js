217b5215811fb1efde736afc15890d19a91a08bf
Teplov <vladimir.teplov@gmail.com>
2021-01-01 18:25:40 +0200

    fix sidebars

--- static/js/headers-list.js
+++ static/js/headers-list.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     var headers = $('.post').find("h1, h2, h3, h4, h5, h6");
-    console.log(headers);
-    headers.each(function(){
-        var title = $(this).text();
-        var id = $(this).attr('id');
-        $('#headers-list').append('<li><a class="underline" href="#'+id+'">'+title+'</a></li>')
-    });
+    if (headers.length > 0) {
+        $('.left-sidebar').show();
+        headers.each(function(){
+            var title = $(this).text();
+            var id = $(this).attr('id');
+            $('#headers-list').append('<li><a class="underline" href="#'+id+'">'+title+'</a></li>')
+        });
+    }
 });
