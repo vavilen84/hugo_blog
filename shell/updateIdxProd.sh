a29c07c7c73be6eda9b02d0d7a3e15a1e2165e79
Teplov <vladimir.teplov@gmail.com>
2021-01-19 22:17:49 +0200

    fix shell command

--- shell/updateIdxProd.sh
+++ shell/updateIdxProd.sh
@@ -1,2 +1,2 @@
-IDX_NAME=blog_local_ LANG=en ENV_FILE=.env.prod node updateIdx/src/index.js
-IDX_NAME=blog_local_ LANG=ru ENV_FILE=.env.prod node updateIdx/src/index.js
+IDX_NAME=blog_production_ LANG=en ENV_FILE=.env.prod node updateIdx/src/index.js
+IDX_NAME=blog_production_ LANG=ru ENV_FILE=.env.prod node updateIdx/src/index.js
